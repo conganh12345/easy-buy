925e27b267b6afeffc6de7786540c58f05123085
conganh12345 <anhvca1234@gmail.com>
2024-10-06 21:30:47 +0700

    Add project files.

---
+++ EasyBuy-Frontend-Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyBuy_Frontend_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ EasyBuy-Frontend-Customer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyBuy_Frontend_Customer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
