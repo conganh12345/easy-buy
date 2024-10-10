3d936e6576f21b071caa1dd2715cf8bac9708c49
Quốc Bảo <huuquocbao0903@gmail.com>
2024-10-10 17:17:34 +0700

    feature/update_supplier_admin

--- EasyBuy-Backend/Data/MyDbContext.cs
+++ EasyBuy-Backend/Data/MyDbContext.cs
@@ -12,10 +12,12 @@ namespace EasyBuy_Backend.Data
         }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new SupplierConfiguration());
         }      
     }
 }
---
+++ EasyBuy-Backend/Models/Supplier.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyBuy_Backend.Models
+{
+    public class Supplier
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; } // Id của nhà cung cấp
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } // Tên nhà cung cấp
+
+        [Required]
+        [StringLength(10)]
+        public string NumberPhone { get; set; } // Số điện thoại nhà cung cấp
+
+        [Required]
+        [StringLength(200)]
+        public string Address { get; set; } // Địa chỉ nhà cung cấp
+
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; } // Email nhà cung cấp
+
+        [Required]
+        public byte Status { get; set; } // Trạng thái nhà cung cấp (0 hoặc 1)
+    }
+}
