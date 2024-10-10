2511a14b62cc543f089e686f3ad4cc9bb7e73743
conganh12345 <anhvca1234@gmail.com>
2024-10-10 15:34:06 +0700

    refactor

--- EasyBuy-Backend/Repositories/IRepository.cs
+++ EasyBuy-Backend/Repositories/IRepository.cs
@@ -3,9 +3,13 @@
     public interface IRepository<T> where T : class
     {
         IEnumerable<T> GetAll();
+
         T GetById(int id);
-        void Create(T entity);
-        void Update(T entity);
-        void Delete(T entity);
+
+        bool Create(T entity);
+
+		bool Update(T entity);
+
+		bool Delete(T entity);
     }
 }
