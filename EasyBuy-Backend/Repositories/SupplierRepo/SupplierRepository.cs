5ae61d5eb75dec05bc4821fc67e29762ef1c26e9
phuc <nguyenvanminhphuc17@gmail.com>
2024-10-16 13:27:03 +0700

    a

---
+++ EasyBuy-Backend/Repositories/SupplierRepo/SupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using EasyBuy_Backend.Data;
+using EasyBuy_Backend.Models;
+
+namespace EasyBuy_Backend.Repositories.SupplierRepo
+{
+	public class SupplierRepository : Repository<Supplier>, ISupplierRepository
+	{
+		public SupplierRepository(MyDbContext context) : base(context)
+		{
+			//
+		}
+	}
+}
---
+++ EasyBuy-Frontend-Admin/RouteConfigs/Modules/Supplier.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyBuy_Frontend_Admin.RouteConfigs.Modules
+{
+    public class Supplier
+    {
+        public static void RegisterRoutes(IEndpointRouteBuilder endpoints)
+        {
+            endpoints.MapControllerRoute(
+                name: "supplier",
+                pattern: "{controller=Supplier}/{action=Index}/{id?}");
+        }
+    }
+}
