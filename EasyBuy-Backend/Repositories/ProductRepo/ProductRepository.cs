8b2ca69a953757fee449983067c45c3826bcd3cf
kimtien03 <volekimtien@gmail.com>
2024-10-16 16:47:16 +0700

    sger

---
+++ EasyBuy-Backend/Repositories/ProductRepo/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using EasyBuy_Backend.Data;
+using EasyBuy_Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyBuy_Backend.Repositories.ProductRepo;
+public class ProductRepository : Repository<Product>, IProductRepository
+{
+    private readonly MyDbContext _context;
+
+        public ProductRepository(MyDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+    //public async Task<Product> GetByEmailAsync(string email)
+    //{
+    //    return await _context.Set<Product>().FirstOrDefaultAsync(u => u.Email == email);
+    //}
+
+    //public async Task<Product> GetProductIdAsStringAsync(string id)
+    //{
+    //    return await _context.Set<Product>().FirstOrDefaultAsync(u => u.Id == id);
+    //}
+}
