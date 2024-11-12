d86e804a2e9ba971c6b9d06dcfbaefa114d669ad
kimtien03 <volekimtien@gmail.com>
2024-11-12 10:24:24 +0700

    update_token

--- EasyBuy-Backend/Program.cs
+++ EasyBuy-Backend/Program.cs
@@ -1,9 +1,13 @@
-﻿using EasyBuy_Backend.Data;
+using EasyBuy_Backend.Data;
 using EasyBuy_Backend.Models;
 using EasyBuy_Backend.Repositories;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using EasyBuy_Backend.Services;
+using EasyBuy_Backend.Services.AuthSvc;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +38,34 @@ builder.Services.AddDbContext<MyDbContext>(options =>
 
 builder.Services.AddRepositories();
 
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+// Thêm các dịch vụ như Authentication và JWT Bearer
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidIssuer = builder.Configuration["JWT:Issuer"],
+		ValidateAudience = true,
+		ValidAudience = builder.Configuration["JWT:Audience"],
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(
+			System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+		),
+		ValidateLifetime = true
+	};
+});
+
+builder.Services.AddAuthorization(options => {
+	options.AddPolicy("Admin", policy => policy.RequireClaim("role", "admin"));
+	options.AddPolicy("Customer", policy => policy.RequireClaim("role", "customer"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,7 +78,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 // Thêm dòng này để áp dụng chính sách CORS
 app.UseCors("AllowAllOrigins");
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- EasyBuy-Backend/Services/AuthSvc/IAuthService.cs
+++ EasyBuy-Backend/Services/AuthSvc/IAuthService.cs
@@ -1,10 +1,10 @@
-﻿using EasyBuy_Backend.Models;
+using EasyBuy_Backend.Models;
 
 namespace EasyBuy_Backend.Services.AuthSvc
 {
     public interface IAuthService
     {
-        Task<bool> Register(User user);
-        bool Login(string email, string password);
-    }
+        Task<string> Register(User user);
+		string Login(string email, string password);
+	}
 }
